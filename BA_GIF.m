18628772a35920fd49e4b1c7de7b3c22cac8e5af
FanChiMao <qaz5517359@gmail.com>
2021-09-15 20:19:56 +0800

    Update BA_GIF.m

--- BA_GIF.m
+++ BA_GIF.m
@@ -2,11 +2,19 @@ clc
 clear all
 close all
 
+% Degraded image path
  noise = imread('D:\NCHU\碩士論文\deraindrop_experiment\result images\data\105_rain.jpg');
+
+% Restored image path
 result = imread('D:\NCHU\碩士論文\deraindrop_experiment\result images\our\105_Our.png');
+
+% Final save path
 output = 'C:\Users\Lab722 BX\Desktop\GIF\';
+
+% each pixel per frame
 step = 10;
 
+%Start generate image%
 [h, w, c] = size(noise);
 count = 1;
 imwrite(noise,[output num2str(count) '.png']);
